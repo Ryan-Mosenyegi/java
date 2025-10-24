4e2564f731c9914ecac8816f46438f2ea99d6202
cse24-103 <cse24-103@thuto.bac.ac.bw>
2025-10-25 00:37:50 +0200

    Refactor Account class to use Customer object

--- src/main/java/com/mycompany/app/Account.java
+++ src/main/java/com/mycompany/app/Account.java
@@ -1,46 +1,23 @@
 package com.mycompany.app;
 
-//Abstract class Account//
 public abstract class Account {
     protected String accountNumber;
-    protected double balance;
+    protected Customer customer;
     protected String branch;
-    protected Customer owner;
+    protected double balance;
 
-    public Account(String accountNumber, Customer owner, String branch) {
+    public Account(String accountNumber, Customer customer, String branch) {
         this.accountNumber = accountNumber;
-        this.owner = owner;
+        this.customer = customer;
         this.branch = branch;
         this.balance = 0.0;
     }
 
-    // Overloaded deposit methods
     public void deposit(double amount) {
         balance += amount;
-        System.out.println("Deposited " + amount + " BWP.");
     }
 
-    public void deposit(double amount, String source) {
-        balance += amount;
-        System.out.println("Deposited " + amount + " BWP from " + source + ".");
-    }
-
-    // Abstract method to be implemented by subclasses
-    public abstract void withdraw(double amount);
-
-    public double getBalance() {
-        return balance;
-    }
-
-    public String getAccountNumber() {
-        return accountNumber;
-    }
-
-    public String getBranch() {
-        return branch;
-    }
-
-    public Customer getOwner() {
-        return owner;
-    }
+    public String getAccountNumber() { return accountNumber; }
+    public String getBranch() { return branch; }
+    public double getBalance() { return balance; }
 }
